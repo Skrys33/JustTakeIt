0a676c32c44a48f67cfaac077109f3210d552477
DevArd <32262877+DevArd@users.noreply.github.com>
2018-05-02 12:06:39 +0200

    callApi

--- config/api.js
+++ config/api.js
@@ -1,5 +1,19 @@
-const https = require('https')
+const request = require('request')
 
 class Api {
     
+    static callApi(url){
+        request(url, function (error, response, body) {
+            
+            if (!error) {
+                console.log(body)
+            }else{
+                console.log(error)                
+            }
+        })
+    }
 }
+
+Api.callApi('http://api.allocine.fr/rest/v3/search?partner=QUNXZWItQWxsb0Npbuk&filter=movie,theater,person,news,tvseries&count=5&page=1&q=avatar&format=json')
+
+module.exports = Api

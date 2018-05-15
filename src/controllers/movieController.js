76a336bb7e1fef4614bc1e788330075592931d65
DevArd <32262877+DevArd@users.noreply.github.com>
2018-05-15 14:01:14 +0200

    promise call api search film

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const api = require('../../config/api')
 
-router.get('/search', (req, res, next) => {
+router.get('/search/:name', async (req, res, next) => {
+    const result = JSON.parse(await api.findFilmByName(req.params.name))
     res.format({
         html: () => {res.render('movies/search')},
    })

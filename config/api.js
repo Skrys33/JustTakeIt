54f9e9e92eed2ebdd874ca7b4cdfc54d0e47f3f1
enzo <enzo.sagnelonge@etu.u-bordeaux.fr>
2018-05-22 10:16:07 +0200

    Feature : Recuperation des informations d'un film et son affichage

--- config/api.js
+++ config/api.js
@@ -17,7 +17,6 @@ class Api {
         return new Promise((resolve, reject) => {
             request('https://api.themoviedb.org/3/movie/'+id+'?api_key=5b555b5b0b7c36a19758ce5d94001c06&language=fr', function (error, response, body) {            
                 if (error) return reject(error)
-                console.log('2')
                 const data = body
                 resolve(data)
             })
--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -6,21 +6,24 @@ app.use(bodyParser.json())
  
 app.get('/movieDetails/:id', async (req, res, next) => {
     const detailsFilm =JSON.parse( await api.findFilmById(req.params.id))
-    console.log('1')
-    console.log( detailsFilm.belongs_to_collection)
-    console.log('-------------')
 
     const movieName = detailsFilm.belongs_to_collection.name
-        
+    const poster = 'https://image.tmdb.org/t/p/w500' + detailsFilm.belongs_to_collection.poster_path
+    const overview = detailsFilm.overview
+    const vote_average = detailsFilm.vote_average
+    const release_date = detailsFilm.release_date
+    
+    const genres = detailsFilm.genres
+    
     res.format({
-        html: () => {res.render('auth/movieDetails', {movieName})},
+        html: () => {res.render('movies/movieDetails', {movieName, poster, overview, vote_average, release_date, genres})},
    })
 })
 
 app.get('/movieDetails', (req, res, next) => {
     const movieName = ''
     res.format({
-        html: () => {res.render('auth/movieDetails', {movieName})},
+        html: () => {res.render('movies/movieDetails', {movieName})},
     })
 })
 

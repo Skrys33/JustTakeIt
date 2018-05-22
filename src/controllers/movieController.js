54b8ef71a9932fe5c3176daa0aa2f0264ce9e976
DevArd <32262877+DevArd@users.noreply.github.com>
2018-05-22 10:43:55 +0200

    View search film

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -3,9 +3,9 @@ const router = express.Router()
 const api = require('../../config/api')
 
 router.get('/search/:name', async (req, res, next) => {
-    const result = JSON.parse(await api.findFilmByName(req.params.name))
+    const films = JSON.parse(await api.findFilmByName(req.params.name))
     res.format({
-        html: () => {res.render('movies/search', {result})},
+        html: () => {res.render('movies/search', {films})},
    })
 });
 

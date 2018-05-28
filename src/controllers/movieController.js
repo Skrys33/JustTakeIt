b5beaf3ffddb7922e35425b17fb8eb22b733cbb3
unknown <kikuiredkill@gmail.com>
2018-05-28 02:51:53 +0200

    choix du film / achat ticket
    achat d un ticket avec sa generation
    gestion en registrement dans la bdd

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -2,24 +2,116 @@ const express = require('express')
 const app = express.Router()
 const api = require('../../config/api')
 const Historical = require("../models/historical")
+const Account = require('../models/account')
+const Movie = require('../models/movie')
+const Ticket = require('../models/ticket')
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
- 
+
+app.post('/movieDetails/:id', (req,res) => {
+    //astuce : regenerer le wallet à chaque fois pour les tests
+    Account.findById('442a9bba-7967-4bfb-8bf3-4950be368215', function(err, account) {
+        account.wallet = 12
+        account.save(function (err, updateAccount) {
+            console.log("wallet test regenerated" + account)
+        })
+    })
+
+    //verifier le nombre de place pour ce film
+    Movie.findOne({'idMovie':req.params.id}, function(err, movie) {
+        if (movie.place > 0) {
+            Account.findOne({idUser: req.session.user._id}, function(err, account) {
+                if(account.wallet < req.body.price){
+                    res.render('home', {errors: ['your wallet is too low !', 'transaction fail !']})
+                }
+                else {
+                    account.wallet -= req.body.price
+                    movie.place -= 1
+                    console.log(movie.place)
+                    account.save(function (err, updateAccount) {    
+                        if (err) return console.error(err)
+                        console.log('update wallet : ' + updateAccount)
+                    })
+                    movie.save(function (err, updateMovie) {    
+                        if (err) return console.error(err)
+                        console.log('update movie place : ' + updateMovie.place)
+                    })
+                    //générer le ticket
+                    let ticket = new Ticket({ idUser: req.session.user._id, idMovie: req.params.id, price: req.body.price })
+                    ticket.save(function (err, newTicket) {
+                        if (err) return console.error(err)
+                        console.log('new ticket géneration : ' + newTicket)
+                        res.render('user/ticket', {tic: newTicket, errors: ['buy confirmed !']})
+                    })
+                }
+            })
+        }
+        else {
+            res.render('home', {errors: ['no place !']})
+        }
+    })
+})
+
 app.get('/movieDetails/:id', async (req, res, next) => {
     const detailsFilm =JSON.parse( await api.findFilmById(req.params.id))
+    //verifier si le film et dans la bdd, => l ajouter => le lire
+    Movie.findOne({ 'idMovie': req.params.id }, function(err, movie) {
+        return movie
+    }).then((movie) => {
+        if(!movie) {
+            
 
-    
-    const movieName = detailsFilm.original_title
-    const poster = 'https://image.tmdb.org/t/p/w500' + detailsFilm.poster_path
-    const overview = detailsFilm.overview
-    const vote_average = detailsFilm.vote_average
-    const release_date = detailsFilm.release_date
-    
-    const genres = detailsFilm.genres
-    
-    res.format({
-        html: () => {res.render('movies/movieDetails', {movieName, poster, overview, vote_average, release_date, genres})},
-   })
+            const movieName = detailsFilm.original_title
+            const poster = 'https://image.tmdb.org/t/p/w500' + detailsFilm.poster_path
+            const overview = detailsFilm.overview
+            const vote_average = detailsFilm.vote_average
+            const release_date = detailsFilm.release_date
+
+            const genres = detailsFilm.genres
+            
+            let genresName = []
+
+            for(let item in genres){
+                //console.log(genres[item].name)
+                genresName[item] = genres[item].name
+            }
+            //console.log(genresName)
+
+            let newMovies = new Movie(
+                {   
+                    name: movieName, 
+                    idMovie: req.params.id,
+                    poster: poster,
+                    overview: overview,
+                    note: vote_average,
+                    genres: genresName,
+                    release_date: release_date,
+                    place:1
+                }
+            )
+            newMovies.save(function (err, movie) {
+                if (err) return console.error(err)
+                console.log('movie : ' + movie)
+            })            
+
+            res.format({
+                html: () => {res.render('movies/movieDetails', {movieName, poster, overview, vote_average, release_date, genresName, id: req.params.id})},
+            })
+        }
+        else {
+            const movieName = movie.name
+            const poster = movie.poster
+            const overview = movie.overview
+            const vote_average = movie.note
+            const release_date = movie.release_date
+            const genres = movie.genres
+            const place = movie.place
+
+            res.format({
+                html: () => {res.render('movies/movieDetails', {movieName, poster, overview, vote_average, release_date, genres, id: req.params.id, place})},
+            })
+        }
+    })
 })
 
 app.get('/movieDetails', (req, res, next) => {
--- src/models/movie.js
+++ src/models/movie.js
@@ -4,10 +4,13 @@ const uuid = require('node-uuid')
 const schemaMovie = Db.mongoose.Schema({ 
   _id: { type: String, default: uuid.v4 }, 
   name: String,
-  director: String,
-  description: String,
-  critical: String,
-  note: Number
+  idMovie: Number,
+  poster: String,
+  overview: String,
+  note: Number,
+  genres: [String],
+  release_date: String,
+  place: Number
 });
 
 const Movies = Db.mongoose.model('Movies', schemaMovie);
